8d60302f59324fa198b210664a90f0fc1ecc873d
sachinreddy1 <sachinreddy1009@gmail.com>
2019-10-08 18:10:58 -0500

    resource path changed, textured added.

--- build/sources/main/java/com/sachinreddy/GeometricAcoustics/GuiExample.java
+++ build/sources/main/java/com/sachinreddy/GeometricAcoustics/GuiExample.java
@@ -10,25 +10,28 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class GuiExample extends Gui
 {
-	private final ResourceLocation bar = new ResourceLocation(GeometricAcousticsCore.modid, "textures/gui/hpbar.png");
-	private final int tex_width = 102, tex_height = 8, bar_width = 100, bar_height = 6;
+	private final ResourceLocation bar = new ResourceLocation(GeometricAcousticsCore.modid, "textures/gui/horizontalaxis.png");
+	private final int tex_width = 245, tex_height = 6;
 	
-	String guiText = "Hello world!";
+	String guiText = "Geometric Acoustics Analytics:";
 	
 	@SubscribeEvent
 	public void renderOverlay(RenderGameOverlayEvent event) {
         if (event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
             Minecraft mc = Minecraft.getMinecraft();
             mc.renderEngine.bindTexture(bar);
-            float oneUnit = (float)bar_width / mc.thePlayer.getMaxHealth();
-            int currentWidth = (int)(oneUnit * mc.thePlayer.getHealth());
-            drawTexturedModalRect(0, 0, 0, 0, tex_width, tex_height);
-            drawTexturedModalRect(1, 0, 1, tex_height, currentWidth, tex_height);
-            //
-			ScaledResolution scaled = new ScaledResolution(mc);
+            
+//            float oneUnit = (float)bar_width / mc.thePlayer.getMaxHealth();
+//            int currentWidth = (int)(oneUnit * mc.thePlayer.getHealth());
+//            drawTexturedModalRect(1, 0, 1, tex_height, currentWidth, tex_height);
+            
+            ScaledResolution scaled = new ScaledResolution(mc);
 			int width = scaled.getScaledWidth();
 			int height = scaled.getScaledHeight();
-			drawCenteredString(mc.fontRendererObj, guiText, width/2, height/2, Integer.parseInt("FFAA00", 16));
+            
+            drawTexturedModalRect((width - tex_width)/2, height * 0.8f, 0, 0, tex_width, tex_height);
+            //
+			drawCenteredString(mc.fontRendererObj, guiText, width/2, height/15, Integer.parseInt("FFFFFF", 16));
         }
     }
 	
