a675ece33f72f20c50b956db533f3208a1d1ce46
sachinreddy1 <sachinreddy1009@gmail.com>
2019-10-08 20:18:55 -0500

    textures added.

--- src/main/java/com/sachinreddy/GeometricAcoustics/GuiExample.java
+++ src/main/java/com/sachinreddy/GeometricAcoustics/GuiExample.java
@@ -10,8 +10,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class GuiExample extends Gui
 {
-	private final ResourceLocation bar = new ResourceLocation(GeometricAcousticsCore.modid, "textures/gui/horizontalaxis.png");
-	private final int tex_width = 245, tex_height = 6;
+	private final ResourceLocation horizontalBar = new ResourceLocation(GeometricAcousticsCore.modid, "textures/gui/horizontalaxis.png");
+	private final ResourceLocation verticalBar = new ResourceLocation(GeometricAcousticsCore.modid, "textures/gui/verticalaxis.png");
+	private int horizontalWidth = 245, horizontalHeight = 6;
+	private int verticalWidth = 6, verticalHeight = 245;
+	private final int verticalPadding = 60, horizontalPadding = 30;
 	
 	String guiText = "Geometric Acoustics Analytics:";
 	
@@ -19,17 +22,20 @@ public class GuiExample extends Gui
 	public void renderOverlay(RenderGameOverlayEvent event) {
         if (event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
             Minecraft mc = Minecraft.getMinecraft();
-            mc.renderEngine.bindTexture(bar);
-            
-//            float oneUnit = (float)bar_width / mc.thePlayer.getMaxHealth();
-//            int currentWidth = (int)(oneUnit * mc.thePlayer.getHealth());
-//            drawTexturedModalRect(1, 0, 1, tex_height, currentWidth, tex_height);
-            
             ScaledResolution scaled = new ScaledResolution(mc);
 			int width = scaled.getScaledWidth();
 			int height = scaled.getScaledHeight();
             
-            drawTexturedModalRect((width - tex_width)/2, height * 0.8f, 0, 0, tex_width, tex_height);
+			// Draw graph axis
+			if (verticalHeight > height)
+				verticalHeight = height - (2 * verticalPadding);
+			if (horizontalWidth > width)
+				horizontalWidth = width - (2 * horizontalPadding);
+			
+			mc.renderEngine.bindTexture(verticalBar);
+			drawTexturedModalRect(horizontalPadding, height - verticalPadding - verticalHeight + 2, 0, 0, verticalWidth, verticalHeight);
+			mc.renderEngine.bindTexture(horizontalBar);
+            drawTexturedModalRect(horizontalPadding + 2, height - verticalPadding, 0, 0, horizontalWidth, horizontalHeight);
             //
 			drawCenteredString(mc.fontRendererObj, guiText, width/2, height/15, Integer.parseInt("FFFFFF", 16));
         }
